c2978aa518d1b2d744b3077f3c0b746d4edb2300
Niall O'Higgins <nialljohiggins@gmail.com>
2010-03-02 06:40:53 -0500

    dont bother trying to free res, we will exit eventually anyway

--- src/mwstart-cmd.c
+++ src/mwstart-cmd.c
@@ -665,7 +665,6 @@ mw_conf_list_start(struct mw_conf_head *root,
 		if (res != NULL) {
 			http_server_init(res->ai_addr, res->ai_addrlen,
 			    http_dispatcher);
-			freeaddrinfo(res);
 		} else {
 			http_server_init(NULL, 0, http_dispatcher);
 		}
